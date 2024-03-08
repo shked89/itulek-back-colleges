63b3444f3089216cd5df334278f093078560257d
nbekposynov <nbekposynov@gmail.com>
2024-03-09 00:29:36 +0500

    Update Pagination

--- app/Http/Controllers/StudentGroupController.php
+++ app/Http/Controllers/StudentGroupController.php
@@ -17,8 +17,16 @@ class StudentGroupController extends Controller
     public function indexStudyGroup(Request $request)
     {
         $collegeId = $request->query('college_id');
-        $result = $this->studentGroupService->getAllStudyGroups($collegeId);
-
+    
+        // Получаем параметры для пагинации из запроса или задаем значения по умолчанию
+        $page = $request->query('page', 1); // Если 'page' не указан, используется первая страница
+        $perPage = $request->query('per_page', 10); // Если 'per_page' не указан, по умолчанию выводится 10 элементов
+    
+        // Передаем параметры пагинации в сервис
+        $result = $this->studentGroupService->getAllStudyGroups($collegeId, $page, $perPage);
+    
+        // Возвращаем результат
         return response()->json($result);
     }
+    
 }
--- app/Services/StudentGroupService.php
+++ app/Services/StudentGroupService.php
@@ -20,16 +20,40 @@ class StudentGroupService
         ]);
     }
 
-    public function getAllStudyGroups($collegeId = null)
+    public function getAllStudyGroups($collegeId = null, $page = 1, $perPage = 10)
     {
+        // Проверяем, был ли предоставлен collegeId
+        if (is_null($collegeId)) {
+            // Возвращаем пустой ответ, если collegeId не предоставлен
+            return [
+                'data' => [],
+                'current_page' => $page,
+                'per_page' => $perPage,
+                'total' => 0,
+                'total_pages' => 0
+            ];
+        }
+    
         $response = $this->client->request('GET', 'api/groups/v1/indexStudy-groups', [
             'query' => ['college_id' => $collegeId]
         ]);
-
+    
         $studyGroups = json_decode($response->getBody()->getContents(), true);
-
-        $studyGroups = $this->enrichStudyGroupsWithDepartments($studyGroups);
-        return $this->enrichStudyGroupsWithSpecialities($studyGroups);
+    
+        // Вычисляем смещение на основе текущей страницы и количества элементов на страницу
+        $offset = ($page - 1) * $perPage;
+    
+        // Применяем смещение и лимит для пагинации
+        $paginatedItems = array_slice($studyGroups, $offset, $perPage);
+    
+        // Подготавливаем и возвращаем данные пагинации
+        return [
+            'data' => $paginatedItems,
+            'current_page' => $page,
+            'per_page' => $perPage,
+            'total' => count($studyGroups),
+            'total_pages' => ceil(count($studyGroups) / $perPage)
+        ];
     }
 
     protected function enrichStudyGroupsWithDepartments($studyGroups)
